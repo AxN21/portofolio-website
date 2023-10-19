3048b46418bcfec4f88a7d4557550d749eca6307
Danciu Alexandru <edialexutu@yahoo.com>
2023-10-19 21:32:08 +0200

    home page

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/* About tabs */
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+        
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('tab__active');
+        });
+
+        target.classList.add('tab__active');
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab__active');
+        });
+
+        tab.classList.add('tab__active');
+    });
+});
